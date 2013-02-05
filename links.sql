93dd9f8dc5525e0a0f2f15c007764022a65b0422
wb7 <lg@wb7.eu>
2013-02-05 16:59:05 +0100

    Initial commit

---
+++ links.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `links` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `short` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `url` text COLLATE utf8_unicode_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+INSERT INTO `links` (`id`, `short`, `url`, `title`) VALUES
+(1, 'wb7', 'http://wb7.eu', 'WB7.eu');
