2d81a7361f76e864dce9df1f9a0d3506b6899a8d
wb7 <lg@wb7.eu>
2013-02-05 17:01:45 +0100

    Added config

---
+++ conf.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+mysql_connect("DB_HOST", "DB_USER", "DB_PASSWORD");
+mysql_select_db("DB");
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-mysql_connect("DB_HOST", "DB_USER", "DB_PASSWORD");
-mysql_select_db("DB");
+
+include('conf.inc.php');
+
 $ergebnis = mysql_query("SELECT * FROM links WHERE short = '" . mysql_real_escape_string($_GET['url']) . "';");
 If (mysql_num_rows($ergebnis) != 0){
 while($row = mysql_fetch_object($ergebnis))
