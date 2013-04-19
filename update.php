1bd961ec37dc1b68f1646457cd15f6356e0a3017
wb7 <lg@wb7.eu>
2013-04-19 20:52:36 +0200

    Fixes issue 1 (confirm issue, resolve issue, resolution=Wird getestet)

---
+++ update.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+
+If you want to upgrade urlShortener, simply call this script. But rename it after upgrade to update.txt!
+
+*/
+
+include('./updater/updater.php');
+
+checkUpdates("urlShortener");
+
+?>
--- updater/updater.php
+++ updater/updater.php
@@ -1,21 +1,25 @@
 <?php
-function checkUpdates($package){
-	//Get file
-	If($handle = ftp_connect("ftp.lg-server.de")){
-	ftp_login($handle, "packages", "packages");
-	ftp_get($handle, $package . "_update.zip", "update.zip", FTP_BINARY);}else{echo "Error! Please report this bug under bugs.lg-server.de!";}
-	//Unzip
-	if(is_file($package . "_update.zip")){
-		$zip = zip_open($package . "_update.zip");
-		while($read = zip_read($zip)){
-			$content = zip_entry_read($read);
-			$name = zip_entry_name($read);
-			$path = dirname($name);
-			if(!is_dir($path))
-				mkdir($path);
-			$handle = fopen($name, "w+");
-			fwrite($handle, $content);
-			fclose($handle);
-		}
+	function checkUpdates($package){
+	
+		//Get file
+	
+			If($handle = ftp_connect("ftp.lg-server.de")){
+			ftp_login($handle, "packages", "packages");
+			ftp_get($handle, $package . "_update.zip", "update.zip", FTP_BINARY);}else{echo "Error! Please report this bug under bugs.lg-server.de!";}
+	
+		//Unzip
+	
+		if(is_file($package . "_update.zip")){
+			$zip = zip_open($package . "_update.zip");
+			while($read = zip_read($zip)){
+				$content = zip_entry_read($read);
+				$name = zip_entry_name($read);
+				$path = dirname($name);
+				if(!is_dir($path)){
+					mkdir($path);
+					$handle = fopen($name, "w+");
+					fwrite($handle, $content);
+					fclose($handle);
+				}
 	}
 ?>
