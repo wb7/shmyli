7b29fb8677871d979ed5ff10ba61ff648706bfc1
wb7 <lg@wb7.eu>
2013-04-19 21:05:08 +0200

    Fixes issue #1 (confirm issue, resolve issue: status=Gefixt)

--- links.sql
+++ links.sql
@@ -7,4 +7,4 @@ CREATE TABLE IF NOT EXISTS `links` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 INSERT INTO `links` (`id`, `short`, `url`, `title`) VALUES
-(1, 'wb7', 'http://wb7.eu', 'WB7.eu');
+(1, 'wb7.eu', 'http://wb7.eu', 'WB7.eu');
