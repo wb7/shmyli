a4692828e6157f1c8857fe7e51aeefd1f693a65b
wb7 <lg@wb7.eu>
2013-04-19 22:09:18 +0200

    Fixes issue #1 (resolve issue: resolution=GELOEST status=Fixed)

--- update.php
+++ update.php
@@ -1,11 +1,8 @@
 <?php
-
 /*
 If you want to upgrade urlShortener, simply call this script. But set the chmod after upgrade to 400!
 */
 
 include('./updater/updater.php');
-
 checkUpdates("urlShortener");
-
 ?>
