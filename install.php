acd7488c70db011fe3e9115a9449f225ad4be97b
wb7 <lg@wb7.eu>
2013-04-20 16:00:49 +0200

    see issue #3 (confirm issue, resolve issue: status=inarbeit, reopen issue)

--- install.php
+++ install.php
@@ -1,8 +1,14 @@
 <?php
-
+error_reporting(0);
 If ($_POST['host'] AND $_POST['user'] AND $_POST['password'] AND $_POST['database']){
 	//WRITE
 	//QUERY
 }
 
 ?>
+
+<form action="" method="post">
+	<input type="text" name="host" placeholder="Host">
+	<input type="text" name="user" placeholder="User">
+	<input type="password" name="password" placeholder="Password">
+	<input type="text" name="database" placeholder="Database">
