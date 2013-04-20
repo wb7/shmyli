6b32280e143c518feca74de287fdb04e234344d6
wb7 <lg@wb7.eu>
2013-04-20 16:25:38 +0200

    Fixes issue #3 (confirm issue, resolve issue: resolution=GELOEST status=erledigt) Finished installer

--- install.php
+++ install.php
@@ -7,15 +7,8 @@ If ($_POST['host'] AND $_POST['user'] AND $_POST['password'] AND $_POST['databas
 	fclose($file);
 	//QUERY
 	include('inc/sql.php');
-	$db->query("CREATE TABLE IF NOT EXISTS `links` (
-`id` int(11) NOT NULL AUTO_INCREMENT,
-`short` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-`url` text COLLATE utf8_unicode_ci NOT NULL,
-`title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2;
-INSERT INTO `links` (`id`, `short`, `url`, `title`) VALUES
-(1, 'wb7.eu', 'http://wb7.eu', 'WB7.eu');");
+	$db->query("CREATE TABLE IF NOT EXISTS `links` (`id` int(11) NOT NULL AUTO_INCREMENT, `short` varchar(255) COLLATE utf8_unicode_ci NOT NULL, `url` text COLLATE utf8_unicode_ci NOT NULL, `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL, PRIMARY KEY (`id`)) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2;");
+	$db->query("INSERT INTO `links` (`id`, `short`, `url`, `title`) VALUES(1, 'wb7.eu', 'http://wb7.eu', 'WB7.eu');");
 }
 
 ?>
