53c5362aa98684fa14b2c38195c8d14dc5eadb1d
wb7 <lg@wb7.eu>
2013-04-21 18:47:49 +0200

    Fixes problem with PHP-Notice (undefined index...)

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php
 
-	If ($_GET['short'] != ""){
+	If(isset($_GET['short'])){
+		If ($_GET['short'] != ""){
 
-		include('inc/sql.php');
-	
-		$result = $db->query("SELECT * FROM `links` WHERE `short` = '" . mysqli_real_escape_string($db, $_GET['short']) . "';");
+			include('inc/sql.php');
 		
-		If ($result->num_rows > 0){
-		
-			while ($row = $result->fetch_object()){
+			$result = $db->query("SELECT * FROM `links` WHERE `short` = '" . mysqli_real_escape_string($db, $_GET['short']) . "';");
 			
-				header("Location: " . $row->url);
+			If ($result->num_rows > 0){
 			
-			}
-			
-		}else{include('404.php');}
+				while ($row = $result->fetch_object()){
+				
+					header("Location: " . $row->url);
+				
+				}
+				
+			}else{include('404.php');}
 
-		$result->close();
-		$db->close();
-		
+			$result->close();
+			$db->close();
+			
+		}else{include('home.php');}
 	}else{include('home.php');}
 ?>
