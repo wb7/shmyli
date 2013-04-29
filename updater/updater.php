059e523f3ec6c2774348a9c0ca2617b12c6b3e45
wb7 <lg@wb7.eu>
2013-04-29 19:07:11 +0200

    Changed updater login

--- updater/updater.php
+++ updater/updater.php
@@ -1,11 +1,11 @@
 <?php
 
-	function checkUpdates($package, $mirror="ftp.lg-server.de"){
+	function checkUpdates($package, $mirror="files.lg-server.de"){
 	
 		//Get file
 	
 			If($handle = ftp_connect($mirror)){
-			ftp_login($handle, "packages", "packages");
+			ftp_login($handle, "files", "files");
 			ftp_get($handle, $package . "_update.zip", "update.zip", FTP_BINARY);}else{echo "Error! Please report this bug on bugs.lg-server.de!";}
 	
 		//Unzip
