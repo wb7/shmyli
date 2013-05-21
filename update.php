68beba4080164fb0790c17a13a4a07bd672c2c1c
wb7 <lg@wb7.eu>
2013-05-21 12:36:43 +0200

    Changed name to shmyli, updated version

--- update.php
+++ update.php
@@ -1,8 +1,8 @@
 <?php
 /*
-If you want to upgrade urlShortener, simply call this script. But set the chmod after upgrade to 400!
+If you want to upgrade SHortMYLInk, simply call this script. But set the chmod after upgrade to 400!
 */
 
 include('./updater/updater.php');
-checkUpdates("urlShortener");
+checkUpdates("shmyli");
 ?>
